889beac3aa1f31488dd5923959cd2fceabe447c4
judy <judy@spiekerpoint.com>
2014-11-13 14:45:12 -0700

    Add reports/OpenedMethod

---
+++ src/main/java/com/ecwid/mailchimp/method/v2_0/reports/OpenedMethod.java
@@ -0,0 +1,37 @@
+package com.ecwid.mailchimp.method.v2_0.reports;
+
+import com.ecwid.mailchimp.MailChimpAPIVersion;
+import com.ecwid.mailchimp.MailChimpMethod;
+import com.ecwid.mailchimp.MailChimpObject;
+import com.ecwid.mailchimp.method.v2_0.lists.DummyResult;
+
+/**
+ *
+ * @author Judy Ng <judy@spiekerpoint.com>
+ */
+@MailChimpMethod.Method(name = "reports/opened", version = MailChimpAPIVersion.v2_0)
+public class OpenedMethod extends MailChimpMethod<DummyResult> {
+	
+	@Field
+	public String cid;
+	
+	@Field 
+	public Opts opts;
+	
+	public static class Opts extends MailChimpObject {
+		
+		@Field
+		public Integer start;
+		
+		@Field
+		public Integer limit;
+		
+        @Field
+        public String sort_field;
+
+        @Field
+        public String sort_dir;
+	}
+	
+	
+}
